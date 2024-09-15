04d9371e154ddd7549f42b39e34f16d3ffef1ee7
Fatih Durmaz <fatih.durmaz@outlook.com>
2024-09-15 23:26:43 +0300

    Initial Commit

---
+++ Localization Tutorial/Localization_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Localization_TutorialApp.swift
+//  Localization Tutorial
+//
+//  Created by Fatih Durmaz on 15.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Localization_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
